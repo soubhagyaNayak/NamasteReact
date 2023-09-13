a57fbacd01f91508cd0aa45b4a36a8300c45dd41
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2023-09-13 22:24:28 +0530

    episode01

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# namstea react 
---
+++ index.js
@@ -0,0 +1,42 @@
+const heading=React.createElement("h1",{},"hii bibek")
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // const heading= React.createElement("h1",{
+    // //     id:"heading",xyz:"abc"
+    // // },"Hello duniya waloo");
+    // // const root=ReactDOM.createRoot(document.getElementById("root"));
+
+    // // root.render(heading);
+
+
+    // const heading=React.createElement("div",{
+    //     id:"parent"},
+    //     React.createElement("div",{id:"child"},
+    //       [React.createElement("h1",{},"i am a don"), React.createElement("h2",{},"soubhagya") ], 
+    //        ),
+    
+    //     React.createElement("div",{id:"child2"},
+    //     [React.createElement("h1",{},"i am a don"), React.createElement("h2",{},"soubhagya") ], 
+    //      )
+    //   );
+
+    //     const root=ReactDOM.createRoot(document.getElementById("root"));
+
+    //     root.render(heading);
+
+
+
